f8e36f578776203556bc3d94cc07dc78e9a7a6e8
Michael Munger <michael.munger1@uqac.ca>
2022-11-25 15:16:28 -0500

    Move web to SWARM

--- SWARM/config.py
+++ SWARM/config.py
@@ -4,7 +4,7 @@ basedir = path.abspath(path.dirname(__file__))
 
 class Config:
     STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = '/templates'
+    TEMPLATES_FOLDER = '/main/templates'
     FLASK_APP = 'run.py'
     SECRET_KEY = "super secret key"
 
@@ -13,16 +13,24 @@ class ProdConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(Config):
     FLASK_ENV = 'development'
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class DB():
+class DB:
     MySQL_API_URL='http://20.48.237.189:5000'
+    MySQL_URL = '20.48.237.189'
+    IoT_ip = "20.104.197.37"
+    IoT_port = "6667"
+    IoT_username = "root"
+    IoT_password = "root"
+    IoT_zone ="UTC-5"
+    need_init = True
     IoT_API_URL='http://20.104.197.37:5000'
-
-
-
-
+    URL = '20.48.237.189'
